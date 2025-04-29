075ba0f01a86c9ce0f8e35e49985ccb8edf1f35a
whitefox980 <ivannestorov80@gmail.com>
2025-04-29 21:43:44 +0200

    Debug: Dodata globalna greška sa alert porukom

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import "@/styles/globals.css";
+import { useEffect } from "react";
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    window.addEventListener("error", (e) => {
+      alert("Greška: " + e.message);
+    });
+
+    window.addEventListener("unhandledrejection", (e) => {
+      alert("Greška u promisu: " + e.reason);
+    });
+  }, []);
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
